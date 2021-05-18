a9a24d8313e57103ad5da276adcd1c5c624fc56c
Panagiotis H.M. Issaris <takis@issaris.com>
2021-05-19 00:06:56 +0200

    Add more tests for the layout

--- test_screen_layout.py
+++ test_screen_layout.py
@@ -30,6 +30,22 @@ def test_get_y_center_position_title():
     assert x == 40
 
 
+def test_get_position_title():
+    layout = Layout(width=1600, height=800)
+    text = Surface(100, 50)
+    x, y = layout.get_position_title(text)
+    assert x == (1600 / 2 - 100 / 2)
+    assert y == (800 / 20 - 50 / 2)
+
+
+def test_get_position_clock():
+    layout = Layout(width=1600, height=800)
+    text = Surface(100, 50)
+    x, y = layout.get_position_clock(text)
+    assert x == (1600 / 2 - 100 / 2)
+    assert y == (2 * 800 / 8 - 50 / 2)
+
+
 def test_get_x_center_position_home():
     layout = Layout(width=1600, height=800)
     x = layout._get_x_center_position_home()
@@ -135,3 +151,51 @@ def test_get_y_position_team_name_home():
     text = Surface(100, 50)
     x, y = layout.get_position_team_name_home(text)
     assert y == (3.5 * 1000 / 8 - 50 / 2)
+
+
+def test_get_position_team_logo_home():
+    layout = Layout(1000, 1000)
+    text = Surface(100, 50)
+    x, y = layout.get_position_team_logo_home(text)
+    assert x == (1000 / 4 - 100 / 2)
+    assert y == (1000 / 4 - 50 / 2)
+
+
+def test_get_position_team_logo_away():
+    layout = Layout(1000, 1000)
+    text = Surface(100, 50)
+    x, y = layout.get_position_team_logo_away(text)
+    assert x == (3 * 1000 / 4 - 100 / 2)
+    assert y == (1000 / 4 - 50 / 2)
+
+
+def test_get_position_team_set_score_home():
+    layout = Layout(1000, 1000)
+    text = Surface(100, 50)
+    x, y = layout.get_position_team_set_score_home(text)
+    assert x == (1000 / 4 - 100 / 2)
+    assert y == (7 * 1000 / 8 - 50 / 2)
+
+
+def test_get_position_team_set_score_away():
+    layout = Layout(1000, 1000)
+    text = Surface(100, 50)
+    x, y = layout.get_position_team_set_score_away(text)
+    assert x == (3 * 1000 / 4 - 100 / 2)
+    assert y == (7 * 1000 / 8 - 50 / 2)
+
+
+def test_get_position_team_score_home():
+    layout = Layout(1000, 1000)
+    text = Surface(100, 50)
+    x, y = layout.get_position_team_score_home(text)
+    assert x == (1000 / 4 - 100 / 2)
+    assert y == (5 * 1000 / 8 - 50 / 2)
+
+
+def test_get_position_team_score_away():
+    layout = Layout(1000, 1000)
+    text = Surface(100, 50)
+    x, y = layout.get_position_team_score_away(text)
+    assert x == (3 * 1000 / 4 - 100 / 2)
+    assert y == (5 * 1000 / 8 - 50 / 2)
