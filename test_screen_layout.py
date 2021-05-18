254eadbbeeaeb4c2cf41cbf862725b4442245bea
Panagiotis H.M. Issaris <takis@issaris.com>
2021-05-18 23:48:54 +0200

    Prefix private methods with underscore

--- test_screen_layout.py
+++ test_screen_layout.py
@@ -20,91 +20,91 @@ class Surface:
 
 def test_get_x_center_position_title():
     layout = Layout(width=1600, height=800)
-    x = layout.get_x_center_position_title()
+    x = layout._get_x_center_position_title()
     assert x == 800
 
 
 def test_get_y_center_position_title():
     layout = Layout(width=1600, height=800)
-    x = layout.get_y_center_position_title()
+    x = layout._get_y_center_position_title()
     assert x == 40
 
 
 def test_get_x_center_position_home():
     layout = Layout(width=1600, height=800)
-    x = layout.get_x_center_position_home()
+    x = layout._get_x_center_position_home()
     assert x == 400
 
 
 def test_get_x_center_position_away():
     layout = Layout(width=1600, height=800)
-    x = layout.get_x_center_position_away()
+    x = layout._get_x_center_position_away()
     assert x == 1200
 
 
 def test_get_x_position_home():
     layout = Layout(width=1600, height=800)
     text = Surface(100, 50)
-    x = layout.get_x_position_home(text)
+    x = layout._get_x_position_home(text)
     assert x == (400 - 100 / 2)
 
 
 def test_get_x_position_away():
     layout = Layout(width=1600, height=800)
     text = Surface(200, 50)
-    x = layout.get_x_position_away(text)
+    x = layout._get_x_position_away(text)
     assert x == (1200 - 200 / 2)
 
 
 def test_get_team_name_y_center_position():
     layout = Layout(width=1000, height=800)
-    y = layout.get_team_name_y_center_position()
+    y = layout._get_team_name_y_center_position()
     assert y == 350
 
 
 def test_get_team_logo_y_center_position():
     layout = Layout(width=1000, height=800)
-    y = layout.get_team_logo_y_center_position()
+    y = layout._get_team_logo_y_center_position()
     assert y == 200
 
 
 def test_get_team_score_y_center_position():
     layout = Layout(width=1000, height=800)
-    y = layout.get_team_score_y_center_position()
+    y = layout._get_team_score_y_center_position()
     assert y == 500
 
 
 def test_get_team_set_score_y_center_position():
     layout = Layout(width=1000, height=800)
-    y = layout.get_team_set_score_y_center_position()
+    y = layout._get_team_set_score_y_center_position()
     assert y == 700
 
 
 def test_get_team_name_y_position():
     layout = Layout(width=1000, height=800)
     text = Surface(100, 50)
-    y = layout.get_team_name_y_position(text)
+    y = layout._get_team_name_y_position(text)
     assert y == (350 - 50 / 2)
 
 
 def test_get_team_logo_y_position():
     layout = Layout(width=1000, height=800)
     logo = Surface(200, 100)
-    y = layout.get_team_logo_y_position(logo)
+    y = layout._get_team_logo_y_position(logo)
     assert y == (200 - 100 / 2)
 
 
 def test_get_team_score_y_position():
     layout = Layout(width=1000, height=800)
     text = Surface(100, 80)
-    y = layout.get_team_score_y_position(text)
+    y = layout._get_team_score_y_position(text)
     assert y == (500 - 80 / 2)
 
 
 def test_get_team_set_score_y_position():
     layout = Layout(width=1000, height=800)
     text = Surface(100, 40)
-    y = layout.get_team_set_score_y_position(text)
+    y = layout._get_team_set_score_y_position(text)
     assert y == (700 - 40 / 2)
 
 
